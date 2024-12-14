7c20acc28efb908c17c2c0f1974b9d6332dd7b63
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-12-14 11:22:01 +0530

    fix refresh token route issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,7 +204,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     return res
       .status(200)
       .cookie('accessToken', accessToken, options)
-      .cookie('refreshToken', refreshToken, options)
+      .cookie('refreshToken', newRefreshToken, options)
       .json(
         new ApiResponse(
           200,
@@ -217,4 +217,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
