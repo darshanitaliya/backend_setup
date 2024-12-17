1a892c550cdc6f7dbf4aecaeec693c563062c6a1
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-12-17 16:26:39 +0530

    create file for aggregation

---
+++ Code_aggregation.js
@@ -0,0 +1,20 @@
+// // Active user
+// [
+//   {
+//     $match: {
+//       isActive: true,
+//     },
+//   },
+//   {
+//     $count: "string",
+//   },
+// ]
+
+// avarage age of all user
+[
+  {
+    $group: {
+      _id: null,
+    },
+  },
+];
