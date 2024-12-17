5b18c3bd03723d049264760751b6a3d056587884
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-12-17 12:06:24 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,8 +151,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: null,
+      $unset: {
+        refreshToken: 1, // this remove the field from document
       },
     },
     { new: true },
