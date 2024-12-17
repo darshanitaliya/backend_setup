b5af63c1d97a7999f4d7fe08b809a83b546f18c9
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-12-17 16:59:51 +0530

    update Code_aggregation file

--- Code_aggregation.js
+++ Code_aggregation.js
@@ -10,11 +10,23 @@
 //   },
 // ]
 
-// avarage age of all user
+// // avarage age of all user
+// [
+//   {
+//     $group: {
+//       _id:null,
+//       averageAge:{
+//         $avg:"$age"
+//       }
+//     },
+//   },
+// ]
+
+// list 5 most common favorite fruits among ther user
 [
   {
     $group: {
-      _id: null,
+      _id: '$favoriteFruit',
     },
   },
 ];
